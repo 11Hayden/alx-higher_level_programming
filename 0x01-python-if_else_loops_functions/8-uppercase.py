3548406b93578809a8ce75fb20512628a362ba21
Hayden <sopuru72@gmail.com>
2023-01-03 14:35:06 -0800

    7,8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def toupper(char):
+    if ord(char) >= 97 and ord(char) <= 122
+        return (ord(char) - 32)
+    else:
+        return ord(char)
+
+def uppercase(string);
+    str_new = ""
+    for char in string:
+        str_new += "%c" % toupper(char)
+    print("{:s}".format(str_new))
