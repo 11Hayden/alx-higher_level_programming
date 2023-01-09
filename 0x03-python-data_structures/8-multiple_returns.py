2a7284f8d288e8aa57b7a82374ce20ebe2b6a88e
Hayden <sopuru72@gmail.com>
2023-01-09 11:21:19 -0800

    8,9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    a_tuple = 0
+    if len(sentence) == 0:
+        a_tuple = 0, "None"
+    else:
+        a_tuple = len(sentence), sentence[0]
+    return a_tuple
