8f555345ffb2551bde911a231d7608c2d273a95b
Hayden <sopuru72@gmail.com>
2023-01-24 12:25:56 -0800

    500

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,8 +33,8 @@ class Square:
 
     def my_print(self):
         """Prints a square made up of #."""
-        for a in range(0, self.__size):
-            [print("#", end="") for b in range(self.__size)]
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
             print("")
