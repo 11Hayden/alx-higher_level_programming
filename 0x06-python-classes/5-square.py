15dfcc8d995af72326d4e9e29f1bedf1c2b9b6ff
Hayden <sopuru72@gmail.com>
2023-01-24 12:11:06 -0800

    554

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,5 +36,5 @@ class Square:
         for a in range(0, self.__size):
             [print("#", end="") for b in range(self.__size)]
             print("")
-        if self.__size = 0:
+        if self.__size == 0:
             print("")
