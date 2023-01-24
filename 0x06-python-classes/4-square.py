2a830943374f6cb55bd78bb56c7663d9989001fb
Hayden <sopuru72@gmail.com>
2023-01-24 11:39:27 -0800

    454

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Reprsents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """To retrieve it."""
+        return(self.__size)
+
+    @property.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of a square."""
+        return(self.__size * self.__size)
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Reprsents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """To retrieve it."""
+        return(self.__size)
+
+    @property.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of a square."""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """Prints a square made up of #."""
+        for a in range(0, self.__size):
+            [print("#", end="") for b in range(self.__size)]
+            print("")
+        if self.__size = 0:
+            print("")
