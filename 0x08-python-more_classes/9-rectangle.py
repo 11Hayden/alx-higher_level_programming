ec86e3e5f977a6cb8849497dee5f17af3d4f5212
Hayden <sopuru72@gmail.com>
2023-01-31 03:35:12 -0800

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -46,6 +46,18 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return Rectangle(size, size)
+
     def area(self):
         """ returns rectangle area"""
         return self.__width * self.__height
